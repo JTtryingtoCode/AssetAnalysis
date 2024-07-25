25270e9d301eb3cabd2e7f80ff8983c7127b57dc
JTtryingtoCode <95494173+JTtryingtoCode@users.noreply.github.com>
2024-07-25 10:28:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -431,4 +431,4 @@ def subscription_cancelled():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True, port=8000)
+    app.run(host='0.0.0.0', port=8000)
