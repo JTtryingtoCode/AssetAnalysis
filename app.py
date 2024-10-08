308bf0768721abd7407aafee5c21728849ba6149
JTtryingtoCode <95494173+JTtryingtoCode@users.noreply.github.com>
2024-10-08 12:18:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -446,4 +446,4 @@ def compute_correlation():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(host='0.0.0.0', debug=True, port=5020)
+    app.run(host='0.0.0.0', debug=True, port=8000)
